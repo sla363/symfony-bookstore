bb80676701a4353463132ee2a86ea1699b97dd8f
slava <slava@example.local>
2023-10-02 22:33:03 +0200

    further register page improvements

--- src/Form/RegisterFormType.php
+++ src/Form/RegisterFormType.php
@@ -6,6 +6,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
 class RegisterFormType extends AbstractType
@@ -15,10 +16,29 @@ class RegisterFormType extends AbstractType
         $builder
             ->add('email', EmailType::class, [
                 'label' => 'Email',
-                'constraints' => new NotBlank(),
+                'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                    'required' => 'required',
+                    'autofocus' => 'autofocus'
+                ],
+                'constraints' => [
+                    new NotBlank(),
+                    new Email()
+                ]
             ])
             ->add('password', PasswordType::class, [
                 'label' => 'Password',
+                'label_attr' => [
+                    'class' => 'form-label',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                    'required' => 'required',
+                    'autofocus' => 'autofocus',
+                ],
                 'constraints' => new NotBlank()
             ]);
     }
